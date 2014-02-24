dbe3c2db7711291f31aaf05f800912a4bda429e9
Alexander I. Zaytsev <hazzik@gmail.com>
2014-02-24 16:59:23 +1300

    Set version 0.0.1

--- src/ReSharper.Reflection/Properties/AssemblyInfo.cs
+++ src/ReSharper.Reflection/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("ReSharper.Reflection")]
